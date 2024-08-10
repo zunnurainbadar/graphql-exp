cc5f6851270932242ed72e5601a971cca99b8b6a
Zunnurain Badar <zunnurainbadar@gmail.com>
2024-08-10 16:06:20 +0500

    Query and Mutation added

--- server.js
+++ server.js
@@ -1,30 +1,66 @@
 var express = require('express');
 var graphqlHTTP = require('express-graphql');
 var { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://127.0.0.1:27017/graphql-exp')
+  .then(() => console.log('Connected!'));
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
-  type Info {
-    first_name:String,
-    last_name:String,
-    age:Int,
-    cnic:String
-  }
   type Query {
-    getMyInfo: Info
+    user(id: Int!): Person
+    users(age: Int): [Person]
+  },
+  type Person {
+    id: Int
+    name: String
+    age: Int
+  },
+  input UserInput {
+    id: Int
+    name: String
+    age: Int
+  },
+  type Mutation {
+    updateUser(input: UserInput): Person
   }
 `);
 
 // The root provides a resolver function for each API endpoint
 var root = {
-  getMyInfo: () => {
+  user: (args) => {
+    console.log('args in getUser ',args)
     return {
-        first_name:"Zunnurain",
-        last_name:"Badar",
-        age:20,
-        cnic:123456789
+        id:1,
+        name:"Zunnurain Badar",
+        age:20
     };
   },
+  users: (args) => {
+    console.log('args in getUsers ',args)
+    return [{
+        id:1,
+        name:"Zunnurain Badar",
+        age:20
+    },{
+      id:2,
+      name:"Zunnurain Badar 1",
+      age:201
+  }];
+  },
+  updateUser: (parent, quote) =>{
+    const id = parent.input.id;
+    const name = parent.input.name;
+    const age = parent.input.age;
+    console.log('id ',id)
+    console.log('name ',name)
+    console.log('age ',age)
+    return {
+      id:id,
+      name:name,
+      age:age
+  }
+  }
 };
 
 var app = express();
